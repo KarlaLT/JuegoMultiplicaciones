3bdb84312c8efdf89d8633d49e0a2bb52acb547a
veronica lopez <karla.veronicalt@hotmail.com>
2021-11-13 13:54:31 -0600

    Juego Multiplaciones

---
+++ JuegoMultiplicaciones/Models/Jugador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoMultiplicaciones.Models
+{
+    public class Jugador
+    {
+        public string Nombre { get; set; }
+        public int Respuesta { get; set; }
+        public DateTime Tiempo { get; set; }
+        public bool Correcto { get; set; }
+    }
+}
